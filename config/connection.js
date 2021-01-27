0b9b2e7664d7da071fae019a8bafaa578bf02048
mohamedahmed-1980 <mohamedahmed0912@hotmail.com>
2021-01-26 22:35:40 -0500

    adjusting to connection to post to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
 var mysql = require("mysql");
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+}else{
+  connection = mysql.createConnection({
+    host:'localhost',
+    user : 'root',
+    password : '1234',
+    database : 'burgers_db'
+  });
+};
+connection.connect();
 
 module.exports = connection;
